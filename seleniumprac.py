d16eadd3ef5c80b1feefc4a8e018c3469ee7d94d
Yamato Tadokoro <miynevfor311jap@gmail.com>
2021-09-19 08:15:49 -0700

    first commit, uploading the selenium practice file

---
+++ seleniumprac.py
@@ -0,0 +1,42 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+PATH = "/Users/yamato0615/Documents/ChromeDrive/chromedriver.exe"
+driver = webdriver.Chrome(PATH)
+
+driver.get("https://techwithtim.net")
+print(driver.title)
+
+search = driver.find_element_by_name("s")
+search.send_keys("test")
+search.send_keys(Keys.RETURN)
+
+# main = driver.find_element_by_id("main")
+# print(main.text)
+
+# explicit wait
+try:
+    main = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "main"))
+    )
+
+    articles = main.find_element_by_tag_name("article")
+    for article in articles:
+        header = article.find_element_by_class_name("entry-summary")
+        print(header.text)
+    print(main.text)
+finally:
+    driver.quit()
+
+
+# ページのソースコード
+print(driver.page_source)
+
+time.sleep(5)
+
+driver.quit()
+
